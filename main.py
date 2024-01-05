0ec4eddbbdbc90bc9de897fe3e117b0c392f9670
2011shao <562797023@qq.com>
2024-01-05 12:10:34 +0800

    提交
    提交

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-from flask import Flask
 
-app = Flask(__name__)
-
+import io
+import requests
+from PIL import Image
+from flask import Flask, request
 from cnocr import CnOcr
-ocr = CnOcr()  # 所有参数都使用默认值
-
-
+OCR_MODEL = CnOcr()
+app = Flask(__name__)
 @app.route('/')
-def hello():
-  img_fp = './aa.jpeg'
-  out = ocr.ocr(img_fp)
-  print(out)
-  # 绘制文本框和文本
-  textArr=''
-  for result in out:
-      text = result['text']
-      score = result['score']
-      position = result['position']
-    #   print('text',text)
-      textArr=textArr+text
+def root():
+    return {"message": "ocr!"}
+@app.route('/ocr',methods=['GET','POST'])
+def ocr():
+    img_url  = request.args.get('image_url')
+    if img_url: 
+      if "http" in img_url:
+        response = requests.get(img_url)  
+        if response.status_code == 200:
+            # 从字节数据中创建图像
+            img = Image.open(io.BytesIO(response.content))        
+            res = OCR_MODEL.ocr(img)
+            textArr=''
+            for result in res:
+                text = result['text']
+                print('text',text)
+                textArr=textArr+text
+            print('dd',textArr)
+            return textArr
+        else:
+            return '图片下载失败'
+       
 
-  return textArr
+      else:
+            return  '图片地址错误'
+    else:
+            return  '位置错误'
+    
+                    
+      
+     
 
 
-
-
-if __name__ == '__main__':
-  print('起送费礼物')
-  app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run()
